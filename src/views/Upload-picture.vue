3fbc87a252a69dc7335e9d43d07baa959d5fe9c5
mil <1363944642@qq.com>
2021-03-20 17:06:40 +0800

    获取文件字段

--- src/views/Upload-picture.vue
+++ src/views/Upload-picture.vue
@@ -9,16 +9,26 @@
           <ValidateInput type="text" placeholder="描述" v-model="describeVal" :rules="describeRule" />
           <ValidateInput type="text" placeholder="标签" v-model="tagVal" :rules="tagRule" />
         </div>
+
         <div class="picture">
           <div class="picture-style">
-            <input class="picture-style_file" type="file" id="file" accept="image/gif,image/jpeg,image/x-png" />
             <label for="file">
               <svg class="icon icon-size-32 icon-size-fill" aria-hidden="true">
                 <use xlink:href="#icon-tubiaolunkuo-"></use>
               </svg>
             </label>
           </div>
+          <ValidateInput
+            class="picture-style_file"
+            type="file"
+            id="file"
+            v-model="pictureVal"
+            :rules="pictureRule"
+            accept="image/gif,image/jpeg,image/x-png,image/jpg"
+            @change="onChangeFile"
+          />
         </div>
+
         <template v-slot:submit>
           <div class="publish">
             <a href="#" class="form-btn">
@@ -51,8 +61,13 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const router = useRouter();
+
+    /**
+     * 表单校验
+     */
     const headlineVal = ref('');
     const describeVal = ref('');
+    const pictureVal = ref('');
     const tagVal = ref('');
     const headlineRule: RulesProp = [
       { type: 'null', message: '标题不能为空' },
@@ -63,37 +78,33 @@ export default defineComponent({
       { type: 'describeMaximum', message: '描述最多25个字符' },
     ];
     const tagRule: RulesProp = [{ type: 'tagMaximum', message: '标签最多5个字符' }];
+    const pictureRule: RulesProp = [{ type: 'null', message: '请上传一张照片' }];
 
+    /**
+     * 表单提交
+     */
     const onFormSubmit = (result: boolean) => {
       if (result) {
-        const { userId } = store.state.user;
-        const { name } = store.state.user;
-        const newPicture = {
-          id: new Date().getTime(),
-          title: headlineVal.value,
-          content: describeVal.value,
-          user: {
-            id: userId,
-            name: name,
-            avatar: 0,
-          },
-          totalComments: 0,
-          file: {
-            id: 22,
-            width: 4000,
-            height: 6000,
-          },
-          tags: [],
-          totalLikes: 0,
-        };
-
-        store.commit('UploadPicture', newPicture);
         router.push('/');
       } else {
         console.log('不通过');
       }
     };
 
+    /**
+     * change事件处理器
+     */
+    const onChangeFile = (event: Event) => {
+      const currenTarget = event.target as HTMLInputElement;
+
+      if (currenTarget.files) {
+        const file = currenTarget.files[0];
+      }
+    };
+
+    /**
+     * 判断是否登录
+     */
     const loginJudge = computed(() => {
       return store.state.user;
     });
@@ -104,9 +115,12 @@ export default defineComponent({
       tagVal,
       headlineRule,
       describeRule,
+      pictureRule,
       tagRule,
       onFormSubmit,
       loginJudge,
+      pictureVal,
+      onChangeFile,
     };
   },
 });
