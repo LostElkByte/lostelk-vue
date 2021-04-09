f9400f7d6c01ab37e69fa0ad65c0d02c759b7eef
mil <1363944642@qq.com>
2021-04-09 17:44:53 +0800

    提示框样式

---
+++ src/components/Affirm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="affirm">
+    <div class="affirm-tooltip">
+      <h3 class="affirm-title">你确定要删除这条评论吗？</h3>
+      <div class="affirm-content">
+        <div class="affirm-button">
+          <button type="button" class="Button">取消</button>
+          <button type="button" class="Button Button-black">确定</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
+<style>
+@import '../style/less/componentsStyle/Affirm.css';
+</style>
