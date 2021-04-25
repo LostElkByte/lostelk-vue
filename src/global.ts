b64467bc43d53bcea95b2b03c3f4786395e0ae51
mil <1363944642@qq.com>
2021-04-25 15:00:02 +0800

    添加注释

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,14 @@
 const lostelkUrl = "https://ovoiii.com";
+/**
+ * 随机精选展示图ID
+ * 横向
+ * 横向图数量
+ * 纵向
+ * 纵向图数量
+ */
 const transverse = [297, 295, 292, 291, 290, 272, 271, 269, 264, 263, 262, 259, 257, 255, 253, 251];
 const transverseLength = transverse.length - 1;
 const longitudinal = [299, 298, 296, 294, 293, 288, 273, 270, 266, 265, 261, 260, 258, 252];
 const longitudinalLength = longitudinal.length - 1;
 export { lostelkUrl, transverse, transverseLength, longitudinal, longitudinalLength }
+
