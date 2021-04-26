6ca6297ed7a781bd32fdbeee920182e5b8fcc8cc
mil <1363944642@qq.com>
2021-04-26 13:02:09 +0800

    样式修改

--- src/components/sidebar/SidebarBox.vue
+++ src/components/sidebar/SidebarBox.vue
@@ -4,7 +4,7 @@
       <div class="sidebar_toolbar_item sidebar_toolbar_item_circle-32" @click="goBack">
         <img src="../../assets/icons/logoTouMing.png" alt="迷鹿" />
       </div>
-      <div class="sidebar_toolbar_item sidebar_toolbar_item_circle-32">
+      <div class="sidebar_toolbar_item sidebar_toolbar_item_circle-32 sidebar_toolbar-avatar">
         <router-link :to="`/@${loginJudge.id}`" v-if="loginJudge.isLogin && loginJudge.avatar">
           <img :src="`${lostelkUrl}/users/${loginJudge.id}/avatar?size=small`" :alt="loginJudge.name" />
         </router-link>
