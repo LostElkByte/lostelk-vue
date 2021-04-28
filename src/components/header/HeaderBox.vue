d7a54b3b93ac60ad2e823d572d5e75b80acaff66
mil <1363944642@qq.com>
2021-04-28 09:20:19 +0800

    修改样式

--- src/components/header/HeaderBox.vue
+++ src/components/header/HeaderBox.vue
@@ -27,8 +27,8 @@
               </span>
             </template>
             <template v-slot:dropdown-content>
-              <router-link to="/Login" class="dropdown-sign-in">Sign in</router-link>
-              <router-link to="/Reglister" class="dropdown-sign-up">Sign up</router-link>
+              <router-link to="/Login" class="dropdown-sign-in">登陆</router-link>
+              <router-link to="/Reglister" class="dropdown-sign-up">加入LostElk</router-link>
             </template>
           </DropDownList>
         </div>
