7547c80a381e6102e9b8187afc3fad1f7b3bbf0c
mil <1363944642@qq.com>
2022-06-27 16:45:54 +0800

    eslint重新配置

--- src/components/globalFun/createTooltip.ts
+++ src/components/globalFun/createTooltip.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import Tooltip from './Tooltip.vue'
-export type TooltipType = 'success' | 'error' | 'default';
+export type TooltipType = 'success' | 'error' | 'default'
 
-const createTooltip = (message: string, type: TooltipType, timeout: number | null) => {
+const createTooltip = (
+  message: string,
+  type: TooltipType,
+  timeout: number | null
+) => {
   // 判断ID为tooltipDom的节点是否存在, 存在就删除
   const tooltipDom = document.getElementById('tooltipDom')
   if (tooltipDom) {
@@ -15,19 +19,18 @@ const createTooltip = (message: string, type: TooltipType, timeout: number | nul
   })
   // 创建tooltipDom节点,并将TooltipInstance(Tooltip)组件 挂载到tooltipDom节点上
   const mountNode = document.createElement('div')
-  mountNode.id = 'tooltipDom';
+  mountNode.id = 'tooltipDom'
   document.body.appendChild(mountNode)
   TooltipInstance.mount(mountNode)
   // 如果定时参数存在 则 设置n秒后自动卸载
   if (timeout) {
     setTimeout(() => {
-      // 卸载Tooltip组件
+      // 卸载Tooltip组件
       TooltipInstance.unmount(mountNode)
       // 删除mountNode节点
       document.body.removeChild(mountNode)
     }, timeout)
   }
-
 }
 
 export default createTooltip
