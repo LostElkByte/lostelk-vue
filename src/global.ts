e74ef6da6a46f47438a1a30f94ddc4de8ce60b22
mil <1363944642@qq.com>
2022-07-08 16:50:31 +0800

    设置服务器接口

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,7 @@
 // 接口服务
-const serveUrl = "http://10.64.6.194:3000";
+const serveUrl = 'https://api.lostelk.cn';
 // 下载路径
-const lostelkUrl = "http://10.64.6.194:3000";
+const lostelkUrl = 'https://api.lostelk.cn';
 /**
  * 固定方式随机展示精选图(暂没有使用,现在的方案是标签名方式展示)
  * 随机精选展示图ID
@@ -10,9 +10,47 @@ const lostelkUrl = "http://10.64.6.194:3000";
  * 纵向
  * 纵向图数量
  */
-const transverse = [297, 295, 292, 291, 290, 272, 271, 269, 264, 263, 262, 259, 257, 255, 253, 251];
+const transverse = [
+  297,
+  295,
+  292,
+  291,
+  290,
+  272,
+  271,
+  269,
+  264,
+  263,
+  262,
+  259,
+  257,
+  255,
+  253,
+  251
+];
 const transverseLength = transverse.length - 1;
-const longitudinal = [299, 298, 296, 294, 293, 288, 273, 270, 266, 265, 261, 260, 258, 252];
+const longitudinal = [
+  299,
+  298,
+  296,
+  294,
+  293,
+  288,
+  273,
+  270,
+  266,
+  265,
+  261,
+  260,
+  258,
+  252
+];
 const longitudinalLength = longitudinal.length - 1;
-export { serveUrl, lostelkUrl, transverse, transverseLength, longitudinal, longitudinalLength }
-
+export {
+  serveUrl,
+  lostelkUrl,
+  transverse,
+  transverseLength,
+  longitudinal,
+  longitudinalLength
+};
