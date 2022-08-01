0ce7f099b8e65e2ff5727712e0033c9ba20f30f6
mil <1363944642@qq.com>
2022-08-01 10:06:39 +0800

    删除无用代码

--- src/global.ts
+++ src/global.ts
@@ -1,56 +1,6 @@
 // 接口服务
-const serveUrl = 'https://api.lostelk.cn';
+const serveUrl = 'http://10.64.6.194:3000';
 // 下载路径
-const lostelkUrl = 'https://api.lostelk.cn';
-/**
- * 固定方式随机展示精选图(暂没有使用,现在的方案是标签名方式展示)
- * 随机精选展示图ID
- * 横向s
- * 横向图数量
- * 纵向
- * 纵向图数量
- */
-const transverse = [
-  297,
-  295,
-  292,
-  291,
-  290,
-  272,
-  271,
-  269,
-  264,
-  263,
-  262,
-  259,
-  257,
-  255,
-  253,
-  251
-];
-const transverseLength = transverse.length - 1;
-const longitudinal = [
-  299,
-  298,
-  296,
-  294,
-  293,
-  288,
-  273,
-  270,
-  266,
-  265,
-  261,
-  260,
-  258,
-  252
-];
-const longitudinalLength = longitudinal.length - 1;
-export {
-  serveUrl,
-  lostelkUrl,
-  transverse,
-  transverseLength,
-  longitudinal,
-  longitudinalLength
-};
+const lostelkUrl = 'http://10.64.6.194:3000';
+
+export { serveUrl, lostelkUrl };
